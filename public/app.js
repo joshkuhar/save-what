55e11465b439118f5a1295e502a1f6c5cafff4ad
Josh Kuhar <jdkuhar@gmail.com>
2016-10-04 20:03:57 -0400

    update

---
+++ public/app.js
@@ -0,0 +1,32 @@
+var MOCK_STATUS_UPDATES = {
+    "statusUpdates": [
+        {
+            "id": "1111111",
+            "text": "Can't believe how much fun I'm having.",
+            "friendId": "aaaaaa",
+            "friendName": "John Doe",
+            "publishedAt": 1470016976609
+        },
+        {
+            "id": "2222222",
+            "text": "Have FOMO? Well you SHOULD!",
+            "friendId": "bbbbbbb",
+            "friendName": "Jane Doe",
+            "publishedAt": 1470012976609
+        },
+        {
+            "id": "333333",
+            "text": "They're giving out immortality and free $$$ where I am.",
+            "friendId": "cccc",
+            "friendName": "Jim Doe",
+            "publishedAt": 1470011976609
+        },
+        {
+            "id": "4444444",
+            "text": "humble brag humble brag humble brag",
+            "friendId": "ddddd",
+            "friendName": "Jackie Doe",
+            "publishedAt": 1470009976609
+        }
+    ]
+};
