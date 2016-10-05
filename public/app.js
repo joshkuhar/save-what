d037adab0428fea3bff6da509bb32fce014111b0
Josh Kuhar <jdkuhar@gmail.com>
2016-10-05 09:35:30 -0400

    update

--- public/app.js
+++ public/app.js
@@ -2,35 +2,47 @@ var MOCK_HISTORY = {
     "History": [
         {
             "id": "1111111",
-            "Name": "Bill",
-            {
+            "Username": "Bill",
+            "Age": "25",
+            "Item": {
                 "Item": "Drinks",
                 "Price": "10"
             }
         },
         {
             "id": "2222222",
-            "Name": "John",
-            {
+            "Username": "John",
+            "Age": "30",
+            "Item": {
                 "Item": "Hat",
                 "Price": "12"
             }
         },
         {
             "id": "333333",
-            "Name": "Lisa",
-            {
+            "Username": "Lisa",
+            "Age": "35",
+            "Item": {
                 "Item": "T.V.",
                 "Price": "400"
             }
         },
         {
             "id": "4444444",
-            "Name": "Tom",
-            {
+            "Username": "Tom",
+            "Age": "40",
+            "Item": {
                 "Item": "Bike",
                 "Price": "200"
             }
         }
     ]
 };
+
+var getItem = MOCK_HISTORY.History[0].Item;
+
+$('input').click(function(){
+    console.log(getItem);
+    $("#item").text(getItem.Item);
+    $("#price").text(getItem.Price);
+});
