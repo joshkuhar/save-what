55f16f2e65c6b4eede271d86af991ad3eb5e6eee
Josh Kuhar <jdkuhar@gmail.com>
2016-10-07 12:22:55 -0400

    update

--- server.js
+++ server.js
@@ -52,28 +52,6 @@ app.get('/a', function(req, res) {
     });
 });
 
-// app.get('/', function(req, res) {
-// 	console.log(req);
-// 	Item.find(function(err, items) {
-// 		if (err) {
-// 			return res.status(500).json({
-// 				message: 'Internal Server Error'
-// 			});
-// 		}
-// 		console.log(req);
-// 		res.json(items);
-// 	});
-// });
-
-// app.get('/a', function(req, res) {
-// 	res.json(data);
-// });
-
-// app.post('/b', function(req, res) {
-// 	// console.log(req);
-// 	console.log(req.body.pass);
-// 	res.status(201).json(data);
-// });
 
 app.post('/b', function(req, res) {	
     Item.create({
@@ -88,19 +66,17 @@ app.post('/b', function(req, res) {
     });
 });
 
+app.delete('/b/:id', function(req, res) {
+    console.log(req);
+    Item.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        // console.log(res);
+        res.status(204).end();
 
-// app.post('/items', function(req, res) {
-//     Item.create({
-//         name: req.body.name
-//     }, function(err, item) {
-//         if (err) {
-//             return res.status(500).json({
-//                 message: 'Internal Server Error'
-//             });
-//         }
-//         res.status(201).json(item);
-//     });
-// });
+    });
+});
 
 
 app.use('*', function(req, res) {
