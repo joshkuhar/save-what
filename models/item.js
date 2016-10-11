9fd949939e83bf2984731483fee53cfa12965edc
Josh Kuhar <jdkuhar@gmail.com>
2016-10-10 21:35:26 -0400

    added name schema

--- models/item.js
+++ models/item.js
@@ -1,14 +1,15 @@
 var mongoose = require('mongoose');
 
-var ItemSchema = new mongoose.Schema({
-	pass: { type: String, required: true }
-});
+// var ItemSchema = new mongoose.Schema({
+// 	pass: { type: String, required: true }
+// });
 var ItemsSchema = new mongoose.Schema({
-	items: {type: Array, required: true}
-
+	items: {type: Array, required: true},
+	name: {type: String, required: true, unique: true}
 });
 
-var Item = mongoose.model('Item', ItemSchema);
+// var Item = mongoose.model('Item', ItemSchema);
 var Items = mongoose.model('Items', ItemsSchema);
 
-module.exports = Item;
+// module.exports = Item;
+module.exports = Items;
