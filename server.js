ccf0d84d09370b4e9c8df5492fe2be6ef5394700
Josh Kuhar <jdkuhar@gmail.com>
2016-10-21 14:49:42 -0400

    mounted middle routes

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var listName = require('./routers/listName/listName');
+var items = require('./routers/items/items');
 
-// var http = require('http');
-// console.log(http);
 
 var config = require('./config');
 
 var app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
 
 var runServer = function(callback) {
 	mongoose.connect(config.DATABASE_URL, function(err) {
@@ -37,59 +38,60 @@ if (require.main === module) {
 exports.app = app;
 exports.runServer = runServer;
 
-var Items = require('./models/item');
+// var Items = require('./models/item');
 
-// var data = {pass: 'Yes'};
+app.use('/a', listName);
+app.use('/b', items);
 
-app.get('/a', function(req, res) {
-    // console.log(req);
-    Items.find( req.query, function(err, items) {
-        if (err) {
-            return res.status(500).json({
-                message: 'Internal Server Error'
-            });
-        }
-        res.status(200).json(items);
-    });
-});
-//
+// app.get('/a', function(req, res) {
+//     // console.log(req);
+//     Items.find( req.query, function(err, items) {
+//         if (err) {
+//             return res.status(500).json({
+//                 message: 'Internal Server Error'
+//             });
+//         }
+//         res.status(200).json(items);
+//     });
+// });
 
-app.post('/b', function(req, res) {	
-    Items.create({
-        items: req.body.items,
-        name: req.body.name
-        // mongoose object id
-    }, function(err, item) {
-        if (err) {
-            return res.status(500).json({
-                message: 'Internal Server Error'
-            });
-        }
-        res.status(201).json(item);
-    });
-});
 
-app.delete('/b/:id', function(req, res) {
-    Items.findByIdAndRemove(req.params.id, function(err) {
-        if (err) {
-            console.log(err);
-        }
-        res.status(204).end();
+// app.post('/b', function(req, res) {	
+//     Items.create({
+//         items: req.body.items,
+//         name: req.body.name
+//         // mongoose object id
+//     }, function(err, item) {
+//         if (err) {
+//             return res.status(500).json({
+//                 message: 'Internal Server Error'
+//             });
+//         }
+//         res.status(201).json(item);
+//     });
+// });
 
-    });
-});
+// app.delete('/b/:id', function(req, res) {
+//     Items.findByIdAndRemove(req.params.id, function(err) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         res.status(204).end();
 
-app.put('/b/:id', function(req, res) {
-    Items.findByIdAndUpdate(req.params.id, {
-        items: req.body.items, 
-        name: req.body.name
-    }, function(err) {
-        if (err) {
-            console.log(err);
-        }
-        res.status(201).end();
-    });
-});
+//     });
+// });
+
+// app.put('/b/:id', function(req, res) {
+//     Items.findByIdAndUpdate(req.params.id, {
+//         items: req.body.items, 
+//         name: req.body.name
+//     }, function(err) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         res.status(201).end();
+//     });
+// });
 
 
 app.use('*', function(req, res) {
