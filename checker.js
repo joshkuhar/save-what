46076fa1b686328787ee1dfcf652365b10681dfc
Josh Kuhar <jdkuhar@gmail.com>
2016-10-21 14:51:40 -0400

    added dev testing files

---
+++ checker.js
@@ -0,0 +1,8 @@
+var middle = require('./middle');
+
+// First way
+// var greeting = new middle.middleTwo("hello world");
+
+
+// Second way
+middle('hello');
---
+++ middle.js
@@ -0,0 +1,9 @@
+var middle = function(word){
+	console.log(word);
+};
+
+// First way
+// exports.middleTwo = middleTwo;
+
+// Second way
+module.exports = middle;
