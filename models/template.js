447754e48946e315d41c157728afb1365a107fa9
Josh Kuhar <jdkuhar@gmail.com>
2016-10-21 14:52:28 -0400

    added template for data model

---
+++ models/template.js
@@ -0,0 +1,18 @@
+ {
+	"listName" : {
+		name: "shopping"
+	},
+
+	"lists" : {
+		items: [
+			"ObjectID(232312)",
+			"ObjectId(234234)"
+		]
+	},
+
+	"item": { 
+		itemName: String,
+		// priceNow: String,
+		priceLater: String
+	}
+}
