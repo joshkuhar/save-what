2bca338b64c18bde75aa1cd79ed9d4aafccc4203
Josh Kuhar <jdkuhar@gmail.com>
2016-10-28 14:30:43 -0400

    cleaned up file

--- server.js
+++ server.js
@@ -43,46 +43,7 @@ exports.runServer = runServer;
 
 app.use('/', categoryName);
 app.use('/', Item);
-// app.use('/b/:id', items);
 
-
-// app.get('/a', function(req, res) {
-//     // console.log(req);
-//     Items.find( req.query, function(err, items) {
-//         if (err) {
-//             return res.status(500).json({
-//                 message: 'Internal Server Error'
-//             });
-//         }
-//         res.status(200).json(items);
-//     });
-// });
-
-
-// app.post('/b', function(req, res) {	
-//     Items.create({
-//         items: req.body.items,
-//         name: req.body.name
-//         // mongoose object id
-//     }, function(err, item) {
-//         if (err) {
-//             return res.status(500).json({
-//                 message: 'Internal Server Error'
-//             });
-//         }
-//         res.status(201).json(item);
-//     });
-// });
-
-// app.delete('/b/:id', function(req, res) {
-//     Items.findByIdAndRemove(req.params.id, function(err) {
-//         if (err) {
-//             console.log(err);
-//         }
-//         res.status(204).end();
-
-//     });
-// });
 
 // app.put('/b/:id', function(req, res) {
 //     Items.findByIdAndUpdate(req.params.id, {
