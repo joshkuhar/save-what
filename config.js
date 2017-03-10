12adad28cc3f583d7b7d30596acebe555be544ec
Josh Kuhar <jdkuhar@gmail.com>
2017-03-10 00:37:19 -0500

    adding config

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            // 'mongodb://localhost/app' :
                             'mongodb://jkuhar:password@ds011314.mlab.com:11314/value-app' :
                             'mongodb://localhost/app-dev');
 exports.PORT = process.env.PORT || 8080;
